66d0a3a5dc397d9fd9473c748df6c7a8b9066129
BertoPolo <bertopolo91@gmail.com>
2024-01-10 21:09:13 +0100

    fix dropdown

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -23,16 +23,14 @@ const Page = () => {
       <div className="text-start">
         <label htmlFor="">Seleccionar moneda</label>
       </div>
-      <select className="select select-bordered w-full max-w-xs">
-        <option disabled selected>
-          Buscar
-        </option>
-        <option onSelect={() => setCoin("bitcoin")}>Bitcoin</option>
-        <option onSelect={() => setCoin("ethereum")}>Ethereum</option>
-        <option onSelect={() => setCoin("litecoin")}>Litecoin</option>
-        <option onSelect={() => setCoin("polygon")}>Polygon</option>
-        <option onSelect={() => setCoin("ripple")}>Ripple</option>
-        <option onSelect={() => setCoin("usdc")}>USD Coin</option>
+      <select className="select select-bordered w-full max-w-xs" onChange={(e) => setCoin(e.target.value)} defaultValue="">
+        <option disabled>Buscar</option>
+        <option value="bitcoin">Bitcoin</option>
+        <option value="ethereum">Ethereum</option>
+        <option value="litecoin">Litecoin</option>
+        <option value="polygon">Polygon</option>
+        <option value="ripple">Ripple</option>
+        <option value="usdc">USD Coin</option>
       </select>
       <br />
       <div className="text-start">
@@ -46,7 +44,7 @@ const Page = () => {
         onChange={(e) => setConcept(e.target.value)}
       />
       <br />
-      <button className="btn btn-primary btn-wide" disabled={price === 0 && !coin && !concept}>
+      <button className="btn btn-primary btn-wide" disabled={price === 0 || !coin || !concept}>
         Continuar
       </button>
     </div>
