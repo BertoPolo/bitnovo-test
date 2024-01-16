5af0b4e1ac1ab68a466b214ad89f86fc42963b18
BertoPolo <bertopolo91@gmail.com>
2024-01-16 20:02:38 +0100

    /success style

--- app/payment/success/page.tsx
+++ app/payment/success/page.tsx
@@ -20,10 +20,12 @@ const PaymentOk = () => {
             <path d="M16.972 6.251c-.967-.538-2.185-.188-2.720.777l-3.713 6.682-2.125-2.125c-.781-.781-2.047-.781-2.828 0-.781.781-.781 2.047 0 2.828l4 4c.378.379.888.587 1.414.587l.277-.020c.621-.087 1.166-.460 1.471-1.009l5-9c.537-.966.189-2.183-.776-2.720z"></path>
           </svg>
         </div>
+
         <h3>¡Pago Completado!</h3>
-        <p className="mb-4">
+        <p className="mb-6 mt-4">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam aspernatur, voluptatum aliquam pariatur ad tempora tempore nobis volupta
         </p>
+
         <Link href={"/payment"}>
           <button className="btn btn-primary w-full">Crear nuevo pago</button>
         </Link>
