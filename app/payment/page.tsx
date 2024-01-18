55dac1100039388c78bbba2fa5c5babbf0e5bb8f
BertoPolo <bertopolo91@gmail.com>
2024-01-18 21:24:59 +0100

    Update page.tsx

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -115,7 +115,12 @@ const PaymentForm = () => {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen ">
+    <div className="relative flex flex-col items-center justify-center min-h-screen ">
+      {isLoading && (
+        <div className="absolute inset-0 flex items-center justify-center bg-white bg-opacity-80">
+          <div className="animate-spin w-16 h-16 border-t-4 border-primary-500 rounded-full"></div>
+        </div>
+      )}
       <div className="shadow-md  p-5 text-center">
         <h3 className="text-xl mb-6">Crear pago</h3>
 
